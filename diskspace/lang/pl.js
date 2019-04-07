770561fb4d9d9bcb057824ec803f58cf951462ec
yoooov <poubelle.maison@gmail.com>
2019-04-07 15:21:01 +0200

    add plugins

---
+++ diskspace/lang/pl.js
@@ -0,0 +1,11 @@
+﻿/*
+ * PLUGIN DISKSPACE
+ *
+ * Polish language file.
+ *
+ * Author:
+ */
+
+ theUILang.diskNotification = "Uwaga! Dysk jest pełny. rTorrent może nie działać poprawnie i żadne dane nie zostaną pobrane, dopóki nie zwolnisz miejsca na dysku.";
+
+thePlugins.get("diskspace").langLoaded();
